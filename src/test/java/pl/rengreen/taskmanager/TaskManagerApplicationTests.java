14b918856e2c81c2ac89b983c9617f1b7eaf49ff
beastangel12 <anzilbista123@gmail.com>
2023-03-19 10:25:35 +0545

    web individual

---
+++ src/test/java/pl/rengreen/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,29 @@
+package pl.rengreen.taskmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+//    @Test
+//    public void login(){
+//
+//    }
+//
+//    @Test
+//    public void profile(){
+//    }
+//
+//    @Test
+//    public void register(){
+//
+//    }
+
+}
